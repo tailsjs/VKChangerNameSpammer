3983b8a59d68bc4183a01ba731d9941582b74d8e
тейлс <49571184+tailsjs@users.noreply.github.com>
2022-02-27 10:20:55 +0400

    небольшой code improvement

--- spam.js
+++ spam.js
@@ -1,44 +1,39 @@
-const config = require("./config.js");
+const {
+	token,
+	group_id,
+	amount,
+	chat_id,
+	time
+} = require("./config.js");
 const { VK } = require("vk-io");
-let vk;
-if(config.group_id < 1){
-	vk = new VK({
-		token: config.token
-	})
-}else{
-	vk = new VK({
-		token: config.token,
-		pollingGroupId: config.group_id
-	})
-};
-if(config.amount < 1 || isNaN(config.amount))return console.error('параметр "amount" некорректен.');
-if(config.time < 1 || isNaN(config.time))return console.error('параметр "time" некорректен.');
-if(config.group_id > 1){
-	vk.api.messages.editChat({
-		title: random(1, 2147483647),
-    		chat_id: config.chat_id	
-	}).catch(function(){
-		return console.error('хуйня.')
-	}).then(function(){
-		console.log('понял, начинаю ебашить.')
-	})
-};
 let changed = 0;
+let vk = new VK(group_id < 1 ? {
+	token: token
+} : {
+	token: token,
+	pollingGroupId: group_id
+})
+
+
+if(amount < 1 || isNaN(amount))return console.error('параметр "amount" некорректен.');
+if(time < 1 || isNaN(time))return console.error('параметр "time" некорректен.');
+
 setInterval(function() {
 	changed += 1;
 	vk.api.messages.editChat({
 		title: random(1, 2147483647),
-		chat_id: config.chat_id	
-	}).catch(function(){
-		return console.error('хуйня.')
+		chat_id: chat_id	
+	}).catch(function(e){
+		console.log(e)
+		return console.error('ошибка!')
 	}).then(function(){
-		console.log(`изменено уже ${changed} раз.`)
+		changed == 1 ? console.log('понял, начинаю ебашить.') : console.log(`изменено уже ${changed} раз.`)
 	});
 	
-	if(changed == config.amount){
+	if(changed == amount){
 		console.log('готово.')
 		break
 	};
-}, config.time*1000);
+}, time*1000);
 
 function random(min, max) {return Math.round(Math.random() * (max - min)) + min}
